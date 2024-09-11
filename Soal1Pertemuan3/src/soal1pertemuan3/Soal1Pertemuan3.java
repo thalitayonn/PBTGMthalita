139446b12110713cfede63d75e6b6dbf7dc0eb3f
thalitayonn <yonlilitha@gmail.com>
2024-09-11 11:05:14 +0700

    Add files via upload

---
+++ Soal1Pertemuan3/src/soal1pertemuan3/Soal1Pertemuan3.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal1pertemuan3;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal1Pertemuan3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 9;
+        float fl = 899;
+        char ch = 'A';
+        boolean b = false;
+        String string = "Hello World";
+        System.out.println("int Var1 " + a);
+        System.out.println("float Var2 " + fl);
+        System.out.println("char Var3 " + ch);
+        System.out.println("boolean Var4 " + b);
+        System.out.println("string Var5 " + string);
+    }
+    
+}
