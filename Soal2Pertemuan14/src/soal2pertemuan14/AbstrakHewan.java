032c9474b1f51a593424e6dc6f59ff2f9005d3d5
thalitayonn <yonlilitha@gmail.com>
2024-09-11 12:00:17 +0700

    Add files via upload

---
+++ Soal2Pertemuan14/src/soal2pertemuan14/AbstrakHewan.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soal2pertemuan14;
+
+/**
+ *
+ * @author dell
+ */
+public abstract class AbstrakHewan {
+    // method abstract
+    public abstract void suara();
+}
---
+++ Soal2Pertemuan14/src/soal2pertemuan14/burung.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soal2pertemuan14;
+
+/**
+ *
+ * @author dell
+ */
+public class burung extends AbstrakHewan {
+    @Override
+    public void suara() {
+        System.out.println("Suara Burung mencicit");
+
+    }
+}
