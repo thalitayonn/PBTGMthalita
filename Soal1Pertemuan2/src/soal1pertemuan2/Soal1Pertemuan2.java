00559edce332b7c294981dea2632a756ae349c1b
thalitayonn <yonlilitha@gmail.com>
2024-09-11 11:01:27 +0700

    Add files via upload

---
+++ Soal1Pertemuan2/src/soal1pertemuan2/Soal1Pertemuan2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal1pertemuan2;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal1Pertemuan2 {
+
+   
+    public static void main(String[] args) {
+        int a = 50;
+        float b = a;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }  
+}
