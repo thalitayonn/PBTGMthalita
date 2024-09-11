5482deb180b3b96fd738ed3ceebdae6e4393f720
thalitayonn <yonlilitha@gmail.com>
2024-09-11 11:53:42 +0700

    Add files via upload

---
+++ Soal1TrycatchPertemuan18/src/soal1trycatchpertemuan18/Soal1TrycatchPertemuan18.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal1trycatchpertemuan18;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal1TrycatchPertemuan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            //may throw exception 
+            
+        } //handling the exception 
+        catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        System.out.println("rest of the code");
+    }
+    
+}
