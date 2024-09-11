c4a0de87008575a497a18f9c53865c7deda9c07c
thalitayonn <yonlilitha@gmail.com>
2024-09-11 11:54:41 +0700

    Add files via upload

---
+++ Soal2Pertemuan3/src/soal2pertemuan3/Soal2Pertemuan3.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal2pertemuan3;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal2Pertemuan3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int NIM = 2110117000;
+        String namaMahasiswa = "Andika Jaya Utama";
+        String kelas = "01SIFE020";
+        double Usia = 19.5;
+        double dl = 1.72;
+
+        System.out.println("NIM : " + NIM);
+        System.out.println("Nama Mahasiswa : " + namaMahasiswa);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("Usia : " +  Usia);
+        System.out.println("Tinggi : " + dl);
+    }
+    
+}
