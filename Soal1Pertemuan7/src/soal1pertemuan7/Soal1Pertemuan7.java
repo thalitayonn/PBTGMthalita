f453c6f2f9141173b60e8c1a44044e683857ae4f
thalitayonn <yonlilitha@gmail.com>
2024-09-11 11:10:50 +0700

    Add files via upload

---
+++ Soal1Pertemuan7/src/soal1pertemuan7/Soal1Pertemuan7.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal1pertemuan7;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal1Pertemuan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Sabtu ");
+                break;
+            case 2:
+                System.out.println("Minggu");
+                break;
+        }
+    }
+    
+}
