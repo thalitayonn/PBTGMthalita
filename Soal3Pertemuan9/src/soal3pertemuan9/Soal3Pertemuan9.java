b9d3ab064899947235619455456e745721af600c
thalitayonn <yonlilitha@gmail.com>
2024-09-11 12:03:25 +0700

    Add files via upload

---
+++ Soal3Pertemuan9/src/soal3pertemuan9/Soal3Pertemuan9.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal3pertemuan9;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal3Pertemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 1;
+        while (i < 6) {
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
