c67154c618c7ede9f5a28c7e0b17764009c8938d
thalitayonn <yonlilitha@gmail.com>
2024-09-11 11:55:15 +0700

    Add files via upload

---
+++ Soal2Pertemuan4/src/soal2pertemuan4/Soal2Pertemuan4.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal2pertemuan4;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal2Pertemuan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here;
+         int x = 6;
+        int w = 5;
+        int y = 8 - x++;
+        int z = 8 - ++w;
+
+        System.out.println("Nilai w: " + w);
+        System.out.println("Nilai x: " + x);
+        System.out.println("Nilai y: " + y);
+        System.out.println("Nilai z: " + z);
+    }
+    
+}
