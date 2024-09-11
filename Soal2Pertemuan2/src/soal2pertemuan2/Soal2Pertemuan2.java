070d641ff9f0e5c4eb748590792541cf73d56450
thalitayonn <yonlilitha@gmail.com>
2024-09-11 11:02:38 +0700

    Add files via upload

---
+++ Soal2Pertemuan2/src/soal2pertemuan2/Soal2Pertemuan2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal2pertemuan2;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal2Pertemuan2 {
+    
+    public static String Makanan;
+    public static final String Minuman = "Jus Jambu";
+    public static void main(String[] args) {
+    // TODO code application logic here
+            Makanan = "Ayam Geprek ";  //Mengisi Value pada variable Makanan
+            System.out.println("Makanan Favorit Saya " + Makanan);//MemanggilVariable Makanan
+            System.out.println("Minuman Favorit Saya "
+                    + Minuman);//Memanggil Variable Minuman
+    }
+}
