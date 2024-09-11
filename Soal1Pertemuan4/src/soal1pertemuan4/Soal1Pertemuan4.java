895a773b7cdab9191b760b64c864b83bcb689d15
thalitayonn <yonlilitha@gmail.com>
2024-09-11 11:07:08 +0700

    Add files via upload

---
+++ Soal1Pertemuan4/src/soal1pertemuan4/Soal1Pertemuan4.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package soal1pertemuan4;
+
+/**
+ *
+ * @author dell
+ */
+public class Soal1Pertemuan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int A = 10;
+        int B = 3;
+        
+        System.out.println("Hasil Pertambahan A + B = "
+                + (A + B));
+        System.out.println("Hasil Pengurangan A - B = "
+                + (A - B));
+        System.out.println("Hasil Perkalian A * B = "
+                + (A * B));
+        System.out.println("Hasil Pembagian A / B: "
+                + (A / B));
+    }
+    
+}
